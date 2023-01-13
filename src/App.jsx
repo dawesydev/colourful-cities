60af02683fdcbd43d1d4ec073f1c7e984e67905d
Dan <danieldawes@protonmail.com>
2023-01-13 14:54:36 +0000

    fixed small screen sizing

--- src/App.jsx
+++ src/App.jsx
@@ -28,18 +28,18 @@ export default function App() {
 
     return (
             <div className="relative scroll-smooth snap-y snap-mandatory h-screen overflow-y-scroll">
-                <h1 className="fixed top-4 left-6 text-5xl font-bold uppercase tracking-wide opacity-80">Colourful Cities</h1>
-                <div className="fixed bottom-4 left-6 w-1/3">
+                <h1 className="fixed top-2 left-3 sm:top-4 sm:left-6 text-5xl font-bold uppercase tracking-wide opacity-60">Colourful Cities</h1>
+                <div className="fixed bottom-2 left-3 sm:bottom-4 sm:left-6 w-2/3 sm:w-1/3 opacity-70">
                     <p>
                         Colourful Cities is a project inspired by the <a href="https://lostin.com/" target="_blank" className="font-bold hover:underline hover:underline-offset-2">LOST iN </a>
                         travel guides. <i>"For people who prefer their itineraries low-key and their souvenirs intangible—unless
                         they’re of the impeccably-designed variety."</i>
                     </p>
+                    <a className="text-xs hover:opacity-50" href="mailto:hello@danieldawes.com">hello@danieldawes.com</a>
                 </div>
-                <p className="fixed top-4 right-6 text-xs opacity-80 hover:opacity-50"><a href="mailto:hello@danieldawes.com">hello@danieldawes.com</a></p>
 
-                <div className="fixed bottom-4 right-6">
-                    <ul className="flex flex-col">
+                <div className="fixed bottom-2 right-3 sm:bottom-4 sm:right-6">
+                    <ul className="flex flex-col opacity-60">
                         {issueElements}
                     </ul>
                 </div>
