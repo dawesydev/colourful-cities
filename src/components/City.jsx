ad42d9fe07f1af8885769fbcccd018776232aa7e
Dan <danieldawes@protonmail.com>
2023-01-13 10:58:56 +0000

    issue with City.jsx

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 export default function City(props) {
     return (
-        <section id={props.id} className={props.sectionCss}>
+        <>
+            <section id={props.id} className={props.sectionCss}>
 
-            <img src={require(props.jpeg)} alt={props.desc} className={props.imgCss} />
+                <img src={props.jpeg} alt={props.desc} className={props.imgCss} />
 
-            {/*<picture>*/}
-            {/*    <source srcSet={props.webp} type="image/webp"  />*/}
-            {/*    <img src={props.jpeg} alt={props.desc}  />*/}
-            {/*</picture>*/}
-        </section>
+                {/*<picture>*/}
+                {/*    <source srcSet={props.webp} type="image/webp"  />*/}
+                {/*    <img src={props.jpeg} alt={props.desc}  />*/}
+                {/*</picture>*/}
+            </section>
+        </>
+
     )
 }
 
