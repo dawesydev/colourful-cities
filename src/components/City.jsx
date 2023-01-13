98a43821dcd7e67da33f7df32d2dbe4126094936
Dan <danieldawes@protonmail.com>
2023-01-13 10:27:40 +0000

    bug fixes

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -3,10 +3,13 @@ import React from "react";
 export default function City(props) {
     return (
         <section id={props.id} className={props.sectionCss}>
-            <picture className={props.imgCss}>
-                <source srcSet={props.webp} type="image/webp"/>
-                <img src={props.jpeg} alt={props.desc}/>
-            </picture>
+
+            <img src={props.jpeg} alt={props.desc} className={props.imgCss} />
+
+            {/*<picture>*/}
+            {/*    <source srcSet={props.webp} type="image/webp"  />*/}
+            {/*    <img src={props.jpeg} alt={props.desc}  />*/}
+            {/*</picture>*/}
         </section>
     )
 }
